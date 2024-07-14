6a796fb06333f3a6467907727b653b886f91cd88
D-Justin-Dsouza <justindsouza80@gmail.com>
2024-07-14 10:18:18 +0530

    Sort function ,and menu added

--- SEM 3/unionintersection.c
+++ SEM 3/unionintersection.c
@@ -7,11 +7,14 @@ struct node {
 };
 
 struct node *createlist(struct node *, int);
+struct node *addatbeg(struct node *, int);
+struct node *addatend(struct node *, int);
 struct node *unionl(struct node *, struct node *, struct node *);
 struct node *intersection(struct node *, struct node *, struct node *);
 struct node *match(struct node *, int data);
 struct node *diff(struct node *, struct node *, struct node *);
 void display(struct node *start);
+struct node *sort(struct node *);
 
 int main() {
     struct node *start1 = NULL;
@@ -20,68 +23,133 @@ int main() {
     struct node *start4 = NULL;
     struct node *start5 = NULL;
     struct node *start6 = NULL;
-    int num, r1, r2;
-    printf("Enter no of nodes for list 1 & list 2:");
-    scanf("%d %d", &r1, &r2);
+    int choice, num, r1, r2;
 
-    printf("\nEnter the data for list 1:");
-    for (int i = 0; i < r1; i++) {
-        scanf("%d", &num);
-        start1 = createlist(start1, num);
+    while(1) {
+        printf("\nMenu:\n");
+        printf("1. Create List 1\n");
+        printf("2. Create List 2\n");
+        printf("3. Display List 1\n");
+        printf("4. Display List 2\n");
+        printf("5. Union of List 1 and List 2\n");
+        printf("6. Intersection of List 1 and List 2\n");
+        printf("7. Difference L1 - L2\n");
+        printf("8. Difference L2 - L1\n");
+        printf("9. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        switch(choice) {
+            case 1:
+                printf("\nEnter number of nodes for List 1: ");
+                scanf("%d", &r1);
+                start1 = NULL;
+                start1 = createlist(start1, r1);
+                break;
+            case 2:
+                printf("\nEnter number of nodes for List 2: ");
+                scanf("%d", &r2);
+                start2 = NULL;
+                start2 = createlist(start2, r2);
+                break;
+            case 3:
+                printf("\nList 1: ");
+                display(start1);
+                break;
+            case 4:
+                printf("\nList 2: ");
+                display(start2);
+                break;
+            case 5:
+                start3 = NULL;
+                start3 = unionl(start1, start2, start3);
+                start3 = sort(start3);
+                printf("\nUnion List: ");
+                display(start3);
+                break;
+            case 6:
+                start4 = NULL;
+                start4 = intersection(start1, start2, start4);
+                start4 = sort(start4);
+                printf("\nIntersection List: ");
+                display(start4);
+                break;
+            case 7:
+                start5 = NULL;
+                start5 = diff(start1, start2, start5);
+                start5 = sort(start5);
+                printf("\nL1 - L2 : ");
+                display(start5);
+                break;
+            case 8:
+                start6 = NULL;
+                start6 = diff(start2, start1, start6);
+                start6 = sort(start6);
+                printf("\nL2 - L1 : ");
+                display(start6);
+                break;
+            case 9:
+                exit(0);
+            default:
+                printf("\nInvalid choice. Please try again.\n");
+        }
     }
 
-    printf("\nEnter the data for list 2:");
-    for (int i = 0; i < r2; i++) {
-        scanf("%d", &num);
-        start2 = createlist(start2, num);
-    }
-
-    start3 = unionl(start1, start2, start3);
-    start4 = intersection(start1, start2, start4);
-    start5 = diff(start1, start2, start5);
-    start6 = diff(start2, start1, start6);
-
-    printf("\nUnion List:");
-    display(start3);
-    printf("\nIntersection List:");
-    display(start4);
-    printf("\nL1 - L2 : ");
-    display(start5);
-    printf("\nL2 - L1 : ");
-    display(start6);
     return 0;
 }
 
-struct node *createlist(struct node *start, int num) {
+struct node *createlist(struct node *start, int n) {
+    int i, info;
+    if (n == 0) {
+        return start;
+    }
+    printf("Enter:");
+    scanf("%d", &info);
+    start = addatbeg(start, info);
+    for (i = 1; i < n; i++) {
+        printf("\nEnter:");
+        scanf("%d", &info);
+        start = addatend(start, info);
+    }
+    return start;
+}
+
+struct node *addatbeg(struct node *start, int info) {
     struct node *temp;
     temp = (struct node *)malloc(sizeof(struct node));
-    temp->data = num;
-    temp->link = NULL;
+    temp->data = info;
+    temp->link = start;
+    start = temp;
+    return start;
+}
 
-    if (start == NULL) {
-        start = temp;
-    }
-    else {
-        struct node *p = start;
-        while (p->link != NULL) {
-            p = p->link;
-        }
-        p->link = temp;
-    }
+struct node *addatend(struct node *start, int info) {
+    struct node *p, *temp;
+    p = start;
+    temp = (struct node *)malloc(sizeof(struct node));
+    temp->data = info;
+    while (p->link != NULL)
+        p = p->link;
+    p->link = temp;
+    temp->link = NULL;
     return start;
 }
 
 struct node *unionl(struct node *start1, struct node *start2, struct node *start3) {
     struct node *temp = start1;
     while (temp != NULL) {
-        start3 = createlist(start3, temp->data);
+        if(start3 == NULL){
+            start3 = addatbeg(start3, temp->data);
+        } else {
+            start3 = addatend(start3, temp->data);
+        }
         temp = temp->link;
     }
 
     temp = start2;
     while (temp != NULL) {
         if (!match(start3, temp->data)) {
-            start3 = createlist(start3, temp->data);
+            start3 = addatend(start3, temp->data);
         }
         temp = temp->link;
     }
@@ -92,7 +160,11 @@ struct node *intersection(struct node *start1, struct node *start2, struct node 
     struct node *temp = start2;
     while (temp != NULL) {
         if (match(start1, temp->data)) {
-            start4 = createlist(start4, temp->data);
+            if(start4 == NULL){
+                start4 = addatbeg(start4, temp->data);
+            } else {
+                start4 = addatend(start4, temp->data);
+            }
         }
         temp = temp->link;
     }
@@ -103,7 +175,11 @@ struct node *diff(struct node *start1, struct node *start2, struct node *start5)
     struct node *temp = start1;
     while (temp != NULL) {
         if (!match(start2, temp->data)) {
-            start5 = createlist(start5, temp->data);
+            if(start5 == NULL){
+                start5 = addatbeg(start5, temp->data);
+            } else {
+                start5 = addatend(start5, temp->data);
+        }
         }
         temp = temp->link;
     }
@@ -123,8 +199,8 @@ struct node *match(struct node *start, int num) {
 
 void display(struct node *start) {
     struct node *temp = start;
-    if(start==NULL){
-        printf("Empty List");
+    if (start == NULL) {
+        printf("Empty List\n");
         return;
     }
     while (temp != NULL) {
@@ -132,4 +208,25 @@ void display(struct node *start) {
         temp = temp->link;
     }
     printf("\n");
+}
+
+struct node *sort(struct node *start) {
+    struct node *i, *j;
+    int temp;
+
+    if (start == NULL || start->link == NULL) {
+        return start;
+    }
+
+    for (i = start; i != NULL; i = i->link) {
+        for (j = i->link; j != NULL; j = j->link) {
+            if (i->data > j->data) {
+                temp = i->data;
+                i->data = j->data;
+                j->data = temp;
+            }
+        }
+    }
+
+    return start;
 }
